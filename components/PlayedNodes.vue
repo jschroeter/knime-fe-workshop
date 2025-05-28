32fe219152bbcf6049785cd78901f57657cf3f11
Michael Schmidt <michael.schmidt@knime.com>
2025-05-28 17:50:07 +0200

    using tag style instead of pills

--- components/PlayedNodes.vue
+++ components/PlayedNodes.vue
@@ -1,20 +1,17 @@
 <script setup lang="ts">
-import { Pill } from "@knime/components";
-
 const gameStore = useGameStore();
 </script>
 
 <template>
   <div class="played-nodes">
-    <!-- TODO remove pill but keep it clickable -->
-    <Pill
+    <NuxtLink
       v-for="playedNode in gameStore.playedNodes"
       :key="playedNode.id"
-      :variant="playedNode.solved ? 'light' : 'error'"
-    >
-      <NuxtLink external target="_blank" :href="playedNode.url">{{
-        playedNode.title
-      }}</NuxtLink></Pill
+      :class="[{ unsolved: !playedNode.solved }]"
+      external
+      target="_blank"
+      :href="playedNode.url"
+      >{{ playedNode.title }}</NuxtLink
     >
   </div>
 </template>
@@ -28,11 +25,36 @@ const gameStore = useGameStore();
   gap: 5px;
   max-width: 800px;
 
-  & .pill {
-    padding: 3px 10px;
+  & a {
+    font-size: 14px;
+    padding: 3px 5px;
+    text-decoration: none;
+    border: 1px solid var(--knime-dove-gray);
+    color: var(--knime-dove-gray);
+    display: inline-block;
+    font-weight: 500;
+    line-height: 17px;
+    margin-bottom: 5px;
+    margin-right: 5px;
+    transition:
+      border-color,
+      background-color 0.05s ease-in-out;
 
-    & a {
-      text-decoration: none;
+    &:hover {
+      background: var(--knime-masala);
+      border-color: var(--knime-masala);
+      color: var(--knime-white);
+    }
+
+    &.unsolved {
+      border: 1px solid var(--knime-coral-dark);
+      color: var(--knime-coral-dark);
+
+      &:hover {
+        background: var(--knime-coral-dark);
+        border-color: var(--knime-coral-dark);
+        color: var(--knime-white);
+      }
     }
   }
 }
