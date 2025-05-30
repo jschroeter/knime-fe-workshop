74c7d2154bbdc296fa272065e1db47f9de10bd16
Jakob Schröter <jakob.schroeter@knime.com>
2025-05-30 11:57:04 +0200

    docs & cleanup

--- stores/game.ts
+++ stores/game.ts
@@ -6,19 +6,23 @@ type PlayedNode = Node & {
 };
 
 export const useGameStore = defineStore("game", () => {
+  /* state */
+
   const node = ref<Node>();
   const playedNodes = ref<PlayedNode[]>([]);
   const points = ref(0);
 
-  /** the score is just a sum of correctly guessed nodes */
+  /* computed state */
+
   const score = computed(() => {
     return playedNodes.value.reduce((acc, n) => acc + (n.solved ? 1 : 0), 0);
   });
 
-  /** level 1 means first 10% of top nodes, level 10 means 100% */
   const level = computed(() => {
     return Math.min(10, Math.floor(score.value / 10) + 1);
   });
+
+  /* actions */
 
   const fetchRandomNode = async () => {
     node.value = await $fetch("/bff/randomNode", {
@@ -37,13 +41,17 @@ export const useGameStore = defineStore("game", () => {
   };
 
   return {
-    addToPlayed,
-    playedNodes,
     node,
-    fetchRandomNode,
+    playedNodes,
+    /** sum of correctly guessed letters */
     points,
-    addPoint,
+    /** sum of nodes with at least one correctly guessed letter */
     score,
+    /** level 1 means first 10% of top nodes, level 10 means 100% */
     level,
+
+    fetchRandomNode,
+    addToPlayed,
+    addPoint,
   };
 });
